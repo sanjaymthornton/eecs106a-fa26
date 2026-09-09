0093f931498ea148cba1972a9341180b53b11e13
Sanjay Thornton <sm3thornton@berkeley.edu>
2026-09-08 21:25:54 -0700

    finished hw1 coding

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -15,5 +15,16 @@ def get_corners(xy, theta, corner1, corner2, corner3, corner4):
     Returns:
         A tuple of four NumPy arrays, each with shape (2, 1).
     """
-    # TODO: implement the rotation and translation for each corner.
+    print(xy[0])
+    print(theta)
+    print(corner1)
+    rotation = np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)]
+    ])
+    corner1 = xy + (rotation @ corner1)
+    corner2 = xy + (rotation @ corner2)
+    corner3 = xy + (rotation @ corner3)
+    corner4 = xy + (rotation @ corner4)
+    return (corner1, corner2, corner3, corner4)
     raise NotImplementedError("Implement get_corners before running the checks.")
